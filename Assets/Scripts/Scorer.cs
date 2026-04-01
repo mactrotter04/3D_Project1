fb55e9b5882885a7047ff35918466773f3958cf9
mactrotter04 <mactrotter04@gmail.com>
2026-04-01 17:21:34 +0200

    added second level
    ive added a second level and im not implimenting confetti when you get the ball

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -1,15 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scorer : MonoBehaviour
 {
     int hits = 0;
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("obsticle"))
+        if (other.gameObject.CompareTag("Win"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("You win!");
+            Invoke(nameof(LoadNextScene), 1f);
+        }
+        else if (other.gameObject.CompareTag("obsticle"))
         {
             hits++;
             Debug.Log("You bumped into Obstilce " + hits + " times");
+            other.gameObject.GetComponent<MeshRenderer>().material.color = Color.lightSteelBlue;
+            other.gameObject.tag = "Hit";
+        }
+        else if (other.gameObject.CompareTag("Death"))
+        {
+            ReloadLevel();
+        }
+        
+    }
+
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load");
         }
     }
 }
